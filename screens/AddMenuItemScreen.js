80cdc6717942f7c307d6bfbb222c621375771071
Ricky Dam <RickyDamOttawa@gmail.com>
2019-07-13 15:16:34 -0400

    Add basic textinput firebase push

--- screens/AddMenuItemScreen.js
+++ screens/AddMenuItemScreen.js
@@ -1,30 +1,46 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import firebase from 'firebase';
 import mainStyles from "../styles/MainStyles";
 import touchableOpacity from "../styles/components/TouchableOpacity";
 
 export default class AddMenuItem extends React.Component {
-  static navigationOptions = () => {
-    return {
-      title: "Add Menu Item",
-      headerRight: (
-        <TouchableOpacity onPress={() => addMenuItem()}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  static navigationOptions = {
+    title: "Add Menu Item",
+  };
+
+  render() {
+    return (
+      <View style={mainStyles.container}>
+        <TextInput
+          onChangeText={(name) => this.setState({name})}
+          placeholder="Name"
+          value={this.state.name}
+        />
+        <TouchableOpacity onPress={() => this.addMenuItem()}>
           <View style={touchableOpacity("#2196F3", 40, 10, 60).view}>
             <Text style={touchableOpacity().text}>Save</Text>
           </View>
         </TouchableOpacity>
-      )
-    }
-  };
-  render() {
-    return (
-      <View style={mainStyles.container}>
-        <Text>This is the AddMenuItemScreen.</Text>
       </View>
     );
   }
-}
 
-function addMenuItem() {
-
+  addMenuItem = () => {
+    let name = this.state.name;
+    firebase.database().ref('Fruits/').push({
+      name
+    }).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(error)
+    });
+  };
 }
