9a22ce8684c058c4e8f96473109245310405e210
Ricky Dam <RickyDamOttawa@gmail.com>
2019-07-29 23:31:31 -0400

    Add button on RestaurantScreen header to go to LayoutScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import mainStyles from "../styles/MainStyles";
+import touchableOpacity from "../styles/components/TouchableOpacity";
 
 export default class RestaurantScreen extends React.Component {
-  static navigationOptions = {
-    title: "Restaurant"
-  };
+  static navigationOptions = ({navigation}) => ({
+    title: "Restaurant",
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate("Layout")}>
+        <View style={touchableOpacity("#9932CC", 40, 10, 60).view}>
+          <Text style={touchableOpacity().text}>Layout</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  });
 
   render() {
     return (
