47273823c08c6cca4dd39007823af866fdffd3a4
Ricky Dam <RickyDamOttawa@gmail.com>
2019-07-31 23:33:06 -0400

    Create animated draggable square table on LayoutScreen

--- screens/LayoutScreen.js
+++ screens/LayoutScreen.js
@@ -1,6 +1,47 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Animated, PanResponder, Text, View } from "react-native";
 import mainStyles from "../styles/MainStyles";
+import layoutStyles from "../styles/LayoutStyles";
+
+class Table extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: new Animated.ValueXY()
+    };
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: (event, gesture) => {
+        this.state.position.setOffset(this.state.position.__getValue());
+        this.state.position.setValue({x: 0, y: 0})
+      },
+      onPanResponderMove: Animated.event([null, {
+        dx: this.state.position.x,
+        dy: this.state.position.y
+      }])
+    });
+  }
+
+  render() {
+    return (
+      <View style={mainStyles.container}>
+        {this.renderTable()}
+      </View>
+    );
+  }
+
+  renderTable() {
+    return (
+      <Animated.View
+        {...this.panResponder.panHandlers}
+        style={[this.state.position.getLayout(), layoutStyles.table]}>
+        <Text>Drag me!</Text>
+      </Animated.View>
+    );
+  }
+}
 
 export default class LayoutScreen extends React.Component {
   static navigationOptions = {
@@ -9,9 +50,7 @@ export default class LayoutScreen extends React.Component {
 
   render() {
     return (
-      <View style={mainStyles.container}>
-        <Text>LayoutScreen</Text>
-      </View>
+      <Table />
     );
   }
 }
---
+++ styles/LayoutStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const layoutStyles = StyleSheet.create({
+  table: {
+    backgroundColor: 'gray',
+    width: 60,
+    height: 60
+  }
+});
+
+module.exports = layoutStyles;
