cb1e6f2da43151caf27fbce486b9e2b68f772aa4
Ricky Dam <RickyDamOttawa@gmail.com>
2019-07-31 23:37:28 -0400

    Move Table class into components directory

--- screens/LayoutScreen.js
+++ screens/LayoutScreen.js
@@ -1,47 +1,5 @@
 import React from "react";
-import { Animated, PanResponder, Text, View } from "react-native";
-import mainStyles from "../styles/MainStyles";
-import layoutStyles from "../styles/LayoutStyles";
-
-class Table extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      position: new Animated.ValueXY()
-    };
-
-    this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: (event, gesture) => {
-        this.state.position.setOffset(this.state.position.__getValue());
-        this.state.position.setValue({x: 0, y: 0})
-      },
-      onPanResponderMove: Animated.event([null, {
-        dx: this.state.position.x,
-        dy: this.state.position.y
-      }])
-    });
-  }
-
-  render() {
-    return (
-      <View style={mainStyles.container}>
-        {this.renderTable()}
-      </View>
-    );
-  }
-
-  renderTable() {
-    return (
-      <Animated.View
-        {...this.panResponder.panHandlers}
-        style={[this.state.position.getLayout(), layoutStyles.table]}>
-        <Text>Drag me!</Text>
-      </Animated.View>
-    );
-  }
-}
+import Table from "../components/Table";
 
 export default class LayoutScreen extends React.Component {
   static navigationOptions = {
