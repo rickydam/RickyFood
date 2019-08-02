c4726d8c72f45fb6d79e1ea77456e6d63f5b599d
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-02 16:34:36 -0400

    Set Table component style to position absolute to prevent View issues

--- styles/LayoutStyles.js
+++ styles/LayoutStyles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from "react-native";
 const layoutStyles = StyleSheet.create({
   table: {
     backgroundColor: "gray",
+    position: "absolute",
     width: 60,
     height: 60
   },
