9cc2aa578cfb0bcbacf4569573f865c9ba9a6794
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-14 18:43:07 -0400

    Rename app and project to RickyFood

--- ios/RickyFoodTests/RickyFoodTests.m
+++ ios/RickyFoodTests/RickyFoodTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RLaguTests : XCTestCase
+@interface RickyFoodTests : XCTestCase
 
 @end
 
-@implementation RLaguTests
+@implementation RickyFoodTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
