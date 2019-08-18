8c252fcda0cfa742298c15b994ef488c294fed1e
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-18 11:15:58 -0400

    Remove headerRight "Register" button from LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,12 @@
 import React from "react";
-import {Text, TouchableOpacity, View} from "react-native"
+import {View} from "react-native"
 import loginStyles from "../styles/LoginStyles";
 import mainStyles from "../styles/MainStyles";
-import touchableOpacity from "../styles/components/TouchableOpacity";
 
 export default class LoginScreen extends React.Component {
-  static navigationOptions = ({navigation}) => ({
-    title: "Login",
-    headerRight: (
-      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-        <View style={touchableOpacity("#2196F3", 40, 10, 65).view}>
-          <Text style={touchableOpacity().text}>Register</Text>
-        </View>
-      </TouchableOpacity>
-    )
-  });
+  static navigationOptions = {
+    title: "Login"
+  };
 
   render() {
     return (
