aa415e38e63069fe1cdd327cd34d783140412960
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-17 23:22:26 -0400

    Add "Register" headerRight button to LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import {View} from "react-native"
+import {Text, TouchableOpacity, View} from "react-native"
 import loginStyles from "../styles/LoginStyles";
 import mainStyles from "../styles/MainStyles";
+import touchableOpacity from "../styles/components/TouchableOpacity";
 
 export default class LoginScreen extends React.Component {
-  static navigationOptions = {
-    title: "Login"
-  };
+  static navigationOptions = ({navigation}) => ({
+    title: "Login",
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+        <View style={touchableOpacity("#2196F3", 40, 10, 65).view}>
+          <Text style={touchableOpacity().text}>Register</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  });
 
   render() {
     return (
