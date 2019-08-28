dd0c150b76c8351756ba98c62a06a0352b423bc6
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-27 22:53:57 -0400

    Change Alert to ToastAndroid for successful registration and login

--- screens/AuthenticationScreen.js
+++ screens/AuthenticationScreen.js
@@ -80,7 +80,7 @@ export default class AuthenticationScreen extends React.Component {
       if(this.state.password != null) {
         firebaseFunctions.registerUser(this.state.email, this.state.password, function(response) {
           if(response === null) {
-            authenticationScreen.createSimpleAlert("Success!", "Registration successful.");
+            ToastAndroid("Registration successful.", ToastAndroid.LONG);
             authenticationScreen.props.navigation.goBack();
           }
           else if(response.code === "auth/weak-password") {
@@ -112,7 +112,7 @@ export default class AuthenticationScreen extends React.Component {
       if(this.state.password != null) {
         firebaseFunctions.loginUser(this.state.email, this.state.password, function(response) {
           if(response === null) {
-            authenticationScreen.createSimpleAlert("Success!", "Login successful.");
+            ToastAndroid("Login successful.", ToastAndroid.LONG);
             authenticationScreen.props.navigation.goBack();
           }
           else if(response.code === "auth/invalid-email") {
