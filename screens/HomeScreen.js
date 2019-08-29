69ecd28d7e72591864b6b7494c6c9671e5b96770
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-29 19:13:24 -0400

    Move HomeScreen authentication functionalities to ProfileScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,10 @@
 import React from "react";
-import {Text, ToastAndroid, View} from "react-native";
+import {Text, View} from "react-native";
 import mainStyles from "../styles/MainStyles";
-import firebaseFunctions from "../firebase/FirebaseFunctions";
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {user: null}
   }
 
   static navigationOptions = ({navigation}) => {
@@ -15,35 +13,13 @@ export default class HomeScreen extends React.Component {
     }
   };
 
-  componentDidMount() {
-    this.reRender = this.props.navigation.addListener("willFocus", () => {
-      this.checkUserAuthentication();
-    });
-    this.props.navigation.setParams({
-      logout: this.logout
-    });
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <View style={mainStyles.container}>
-        {this.state.user ? <Text>{this.state.user.email}</Text> : null}
         <Text>HomeScreen</Text>
       </View>
     );
   };
-
-  checkUserAuthentication = () => {
-    firebaseFunctions.checkUserAuthentication(user => {
-      this.setState({user: user});
-    });
-  };
-
-  logout = () => {
-    let homeScreen = this;
-    firebaseFunctions.logout(function() {
-      ToastAndroid.show("Logout successful.", ToastAndroid.LONG);
-      homeScreen.setState({user: null});
-    })
-  }
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,12 +1,27 @@
 import React from "react";
-import {Text, TouchableOpacity, View} from "react-native";
+import {Text, ToastAndroid, TouchableOpacity, View} from "react-native";
 import mainStyles from "../styles/MainStyles";
 import touchableOpacity from "../styles/components/TouchableOpacity";
+import firebaseFunctions from "../firebase/FirebaseFunctions";
 
 export default class ProfileScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {user: null}
+  }
+
   static navigationOptions = {
     title: "Profile"
   };
+
+  componentDidMount() {
+    this.reRender = this.props.navigation.addListener("willFocus", () => {
+      this.checkUserAuthentication();
+    });
+    this.props.navigation.setParams({
+      logout: this.logout
+    });
+  }
 
   render() {
     return (
@@ -28,7 +43,24 @@ export default class ProfileScreen extends React.Component {
             </View>
           </TouchableOpacity>
         </View>
+        <View>
+          {this.state.user ? <Text>{this.state.user.email}</Text> : null}
+        </View>
       </View>
     );
+  }
+
+  checkUserAuthentication = () => {
+    firebaseFunctions.checkUserAuthentication(user => {
+      this.setState({user: user});
+    });
+  };
+
+  logout = () => {
+    let homeScreen = this;
+    firebaseFunctions.logout(function() {
+      ToastAndroid.show("Logout successful.", ToastAndroid.LONG);
+      homeScreen.setState({user: null});
+    })
   }
 }
