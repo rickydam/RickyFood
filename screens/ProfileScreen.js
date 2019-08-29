736c2e000d2c2982aef708538ce06c0848e2643c
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-29 18:55:36 -0400

    Add authentication buttons to ProfileScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Image, Text, View} from "react-native";
+import {Image, Text, TouchableOpacity, View} from "react-native";
 import mainStyles from "../styles/MainStyles";
+import touchableOpacity from "../styles/components/TouchableOpacity";
 
 const profileIcon = require("../images/icons/profile.png");
 
@@ -16,7 +17,23 @@ export default class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={mainStyles.container}>
-        <Text>ProfileScreen</Text>
+        <View style={mainStyles.row}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Register"})}>
+            <View style={touchableOpacity("#707070", 40, 5, 70).view}>
+              <Text style={touchableOpacity().text}>Register</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Login"})}>
+            <View style={touchableOpacity("#707070", 40, 5, 60).view}>
+              <Text style={touchableOpacity().text}>Login</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.state.params.logout()}>
+            <View style={touchableOpacity("#707070", 40, 5, 60).view}>
+              <Text style={touchableOpacity().text}>Logout</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
