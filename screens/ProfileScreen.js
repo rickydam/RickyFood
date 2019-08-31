883b9f00cb42e70d965f11c9e2bb0bc2a48eaa9d
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-30 21:21:29 -0400

    Hide and show Register/Login/Logout accordingly

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -24,30 +24,40 @@ export default class ProfileScreen extends React.Component {
   }
 
   render() {
-    return (
-      <View style={mainStyles.container}>
-        <View style={mainStyles.row}>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Register"})}>
-            <View style={touchableOpacity("#707070", 40, 5, 70).view}>
-              <Text style={touchableOpacity().text}>Register</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Login"})}>
-            <View style={touchableOpacity("#707070", 40, 5, 60).view}>
-              <Text style={touchableOpacity().text}>Login</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => this.props.navigation.state.params.logout()}>
-            <View style={touchableOpacity("#707070", 40, 5, 60).view}>
-              <Text style={touchableOpacity().text}>Logout</Text>
-            </View>
-          </TouchableOpacity>
+    if(this.state.user) {
+      return (
+        <View style={mainStyles.container}>
+          <View style={mainStyles.row}>
+            <TouchableOpacity onPress={() => this.props.navigation.state.params.logout()}>
+              <View style={touchableOpacity("#707070", 40, 5, 60).view}>
+                <Text style={touchableOpacity().text}>Logout</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View>
+            {this.state.user ? <Text>{this.state.user.email}</Text> : null}
+          </View>
         </View>
-        <View>
-          {this.state.user ? <Text>{this.state.user.email}</Text> : null}
+      );
+    }
+    else {
+      return (
+        <View style={mainStyles.container}>
+          <View style={mainStyles.row}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Register"})}>
+              <View style={touchableOpacity("#707070", 40, 5, 70).view}>
+                <Text style={touchableOpacity().text}>Register</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate("Authentication", {purpose: "Login"})}>
+              <View style={touchableOpacity("#707070", 40, 5, 60).view}>
+                <Text style={touchableOpacity().text}>Login</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    );
+      );
+    }
   }
 
   checkUserAuthentication = () => {
