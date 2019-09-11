0c9a8a1464ffacdc38a37c1120f0b4ec17218997
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-11 19:08:32 -0400

    Add didFocus listener and RestaurantSelector to RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -5,6 +5,7 @@ import touchableOpacity from "../styles/components/TouchableOpacity";
 import firebaseFunctions from "../functions/FirebaseFunctions";
 import Table from "../components/Table";
 import mainFunctions from "../functions/MainFunctions";
+import RestaurantSelector from "../components/RestaurantSelector";
 
 export default class RestaurantScreen extends React.Component {
   constructor(props) {
@@ -25,12 +26,15 @@ export default class RestaurantScreen extends React.Component {
 
   componentDidMount() {
     let restaurantScreen = this;
-    this.reRender = this.props.navigation.addListener("willFocus", () => {
+    this.reRender = this.props.navigation.addListener("didFocus", () => {
       mainFunctions.getItemSelectedRestaurant(function(selectedRestaurant) {
         if(selectedRestaurant !== null) {
           restaurantScreen.setState({selectedRestaurant: selectedRestaurant});
           restaurantScreen.setState({tables: []});
           restaurantScreen.loadTables();
+        }
+        else {
+          restaurantScreen.setState({selectedRestaurant: null});
         }
       });
     });
@@ -41,25 +45,32 @@ export default class RestaurantScreen extends React.Component {
   }
 
   render() {
-    if(this.state.tables.length > 0) {
-      let tables = this.state.tables.map((table) => {
-        return <Table
-          key={table.createdAt}
-          table={table}
-          screen={"RestaurantScreen"}
-        />
-      });
-      return (
-        <View style={mainStyles.container}>
-          {tables}
-        </View>
-      );
+    if(this.state.selectedRestaurant !== null) {
+      if(this.state.tables.length > 0) {
+        let tables = this.state.tables.map((table) => {
+          return <Table
+            key={table.createdAt}
+            table={table}
+            screen={"RestaurantScreen"}
+          />
+        });
+        return (
+          <View style={mainStyles.container}>
+            {tables}
+          </View>
+        );
+      }
+      else {
+        return (
+          <View style={mainStyles.container}>
+            <Text>No restaurant tables to display.</Text>
+          </View>
+        );
+      }
     }
     else {
       return (
-        <View style={mainStyles.container}>
-          <Text>No restaurant tables to display.</Text>
-        </View>
+        <RestaurantSelector nav={this.props.navigation} restaurant={this.setSelectedRestaurant} />
       );
     }
   }
@@ -68,5 +79,10 @@ export default class RestaurantScreen extends React.Component {
     let restaurantScreen = this;
     let tables = await firebaseFunctions.loadTables(restaurantScreen.state.selectedRestaurant.key);
     this.setState({tables: tables});
+  };
+
+  setSelectedRestaurant = (selectedRestaurant) => {
+    this.setState({selectedRestaurant: selectedRestaurant});
+    this.loadTables();
   };
 }
