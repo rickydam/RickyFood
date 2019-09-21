07cdd85c1d05d90292f1654f0c25ee7eeb988962
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-21 11:21:11 -0400

    Add Redux Reducer switch case for setRestaurant

--- redux/reduxReducer.js
+++ redux/reduxReducer.js
@@ -1,13 +1,20 @@
 import {combineReducers} from 'redux';
-import {SET_USER_DATA} from './types';
+import {SET_USER_DATA, SET_RESTAURANT} from './types';
 
-let initialState = {userData: null};
+let initialState = {userData: null, restaurant: null};
 
-const reduxReducer = (state = initialState, action) => {
+const reduxReducer = (state=initialState, action) => {
+  let updatedState;
   switch(action.type) {
     case SET_USER_DATA:
-      let updatedState = {};
+      updatedState = {};
       updatedState.userData = action.payload;
+      updatedState.restaurant = state.restaurant;
+      return updatedState;
+    case SET_RESTAURANT:
+      updatedState = {};
+      updatedState.userData = state.userData;
+      updatedState.restaurant = action.payload;
       return updatedState;
     default:
       return state;
