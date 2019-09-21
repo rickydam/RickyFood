2add15aa64bdbc7f990ee60a68afc5f8e3bba3f5
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-21 11:34:44 -0400

    Remove obsolete HomeScreen state management

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,10 @@ import {Text, TouchableOpacity, View} from 'react-native';
 import mainStyles from '../styles/MainStyles';
 import touchableOpacity from '../styles/components/TouchableOpacity';
 import RestaurantSelector from '../components/RestaurantSelector';
-import mainFunctions from '../functions/MainFunctions';
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {selectedRestaurant: null, user: null, userType: null};
   }
 
   static navigationOptions = ({navigation}) => {
@@ -17,22 +15,12 @@ export default class HomeScreen extends React.Component {
     }
   };
 
-  componentDidMount() {
-    let homeScreen = this;
-    this.reRender = this.props.navigation.addListener('willFocus', () => {
-      mainFunctions.getItemSelectedRestaurant(function(selectedRestaurant) {
-        if(selectedRestaurant !== null) homeScreen.setState({selectedRestaurant: selectedRestaurant});
-      })
-    });
-  }
+  componentDidMount() {}
 
   render() {
     if(this.state.selectedRestaurant !== null) {
       return (
         <View style={mainStyles.container}>
-          <Text>{this.state.selectedRestaurant.name}</Text>
-          <Text>{this.state.selectedRestaurant.owner}</Text>
-          <Text>{this.state.selectedRestaurant.key}</Text>
           <TouchableOpacity onPress={() => this.clearSelectedRestaurant()}>
             <View style={touchableOpacity('#9932CC', 40, 10, 60).view}>
               <Text style={touchableOpacity().text}>Switch</Text>
@@ -48,14 +36,5 @@ export default class HomeScreen extends React.Component {
     }
   };
 
-  setSelectedRestaurant = (selectedRestaurant) => {
-    this.setState({selectedRestaurant: selectedRestaurant});
-  };
-
-  clearSelectedRestaurant = () => {
-    let homeScreen = this;
-    mainFunctions.removeItemSelectedRestaurant(function(err) {
-      if(!err) homeScreen.setState({selectedRestaurant: null});
-    });
-  }
+  clearSelectedRestaurant = () => {};
 }
