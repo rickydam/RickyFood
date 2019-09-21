3167ceb0c0322bd9d9febe589477299f475180af
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-21 11:25:14 -0400

    Add Redux Action for setRestaurant

--- redux/reduxActions.js
+++ redux/reduxActions.js
@@ -1,8 +1,15 @@
-import {SET_USER_DATA} from './types';
+import {SET_USER_DATA, SET_RESTAURANT} from './types';
 
-export const setUserData = userData => (
-  {
+const setUserData = (userData) => ({
     type: SET_USER_DATA,
     payload: userData
-  }
-);
+});
+
+const setRestaurant = (restaurant) => ({
+    type: SET_RESTAURANT,
+    payload: restaurant
+});
+
+export {
+  setUserData
+}
