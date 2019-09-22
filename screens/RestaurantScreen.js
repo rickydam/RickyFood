5394f7face108b3ebb02cb6aa6fbf48ebb0b604b
Ricky Dam <RickyDamOttawa@gmail.com>
2019-09-22 14:46:07 -0400

    Add componentDidUpdate to RestaurantScreen to compare Redux props

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -30,6 +30,35 @@ class RestaurantScreen extends React.Component {
     this.reRender.remove();
   }
 
+  componentDidUpdate(prevProps) {
+    if(this.props.redux.restaurant !== prevProps.redux.restaurant) {
+      if(this.props.redux.restaurant !== null) {
+        // case 1: object and null
+        // restaurant has been selected, load the tables
+        this.loadTables();
+      }
+      else {
+        // case 2: null and object
+        // restaurant cleared, clear the tables
+        this.clearTables();
+      }
+    }
+    else {
+      if(this.props.redux.restaurant !== null && prevProps.redux.restaurant !== null) {
+        // Make sure both, this.props and prevProps, have a non-null restaurant property before accessing it
+        if(this.props.redux.restaurant.key !== prevProps.redux.restaurant.key) {
+          // case 3: object and object
+          // restaurant has been switched, load the tables
+          this.loadTables();
+        }
+      }
+      else {
+        // case 4: null and null
+        // no action required
+      }
+    }
+  }
+
   render() {
     if(this.props.redux.restaurant !== null) {
       if(this.state.tables.length > 0) {
